4cbaafd1389d65c80698bfec253e5517342aac5c
Felix Kröhnert WinWsl <felix.kroehnert@online.de>
2022-11-04 13:20:26 +0100

    dynamic library

--- compile.sh
+++ compile.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-#compile
-gcc -c -Wall -Werror -Wextra src/*.c
-
-#archive
-ar -rcs bin/libcparser.a *.o
-
 #add header
 cp src/*.h bin/
 
+## static
+
+#compile
+gcc -c -Wall -Werror -Wextra src/*.c
+#archive
+ar -rcs bin/libcparser.a *.o
+
+#post-cleanup
+rm *.o
+
+
+## dynamic
+
+#compile
+gcc -fPIC -c -Wall -Werror -Wextra src/*.c
+# create shared object
+gcc -shared -Wl,-soname,libcparser.so -o bin/libcparser.so *.o
 #post-cleanup
 rm *.o
